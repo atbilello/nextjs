517db2a1346caa970f5f8ca8b1eba41f251475bc
atbilello <84088224+atbilello@users.noreply.github.com>
2021-05-12 11:28:59 -0400

    Update index.js
    Updated name to Annemarie and team

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome <a href="https://nextjs.org">Annemarie!</a>
+          Welcome <a href="https://nextjs.org">Annemarie and team!</a>
         </h1>
 
         <p className={styles.description}>
